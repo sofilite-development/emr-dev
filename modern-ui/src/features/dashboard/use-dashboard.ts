7f9bbd45cd04b9bd04f97e5faab8a80bf76b88ee
khalids01 <khalids8774@gmail.com>
2025-06-25 11:35:55 +0600

    new ui build filex added

--- modern-ui/src/features/dashboard/use-dashboard.ts
+++ modern-ui/src/features/dashboard/use-dashboard.ts
@@ -2,7 +2,7 @@ import { api } from "@/lib/api";
 import { endpoints } from "@/constants/endpoints";
 import { queryKeys } from "@/constants/querykeys";
 import { useQuery } from "@tanstack/react-query";
-import { type OverviewResponse } from "./type";
+import { type OverviewResponse } from "./type.d";
 
 export const useDashboard = () => {
     const overviewQuery = useQuery({
